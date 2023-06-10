d97e1cfa91f591a78a561071fde1f47663626843
Vithor Félix <vithorfjm@outlook.com>
2023-06-09 21:50:36 -0300

    Classe service incluída no Projeto

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -3,59 +3,44 @@ package com.example.crud.controllers;
 import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
 import com.example.crud.domain.product.RequestProduct;
+import com.example.crud.services.ProductService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import org.hibernate.action.internal.EntityActionVetoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("product")
 public class ProductController {
     @Autowired
-    private ProductRepository repository;
+    private ProductService productService;
 
     @GetMapping
-    public ResponseEntity getAllProducts() {
-        var allProducts = repository.findAllByActiveTrue();
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> allProducts = productService.getAllProducts();
         return ResponseEntity.ok(allProducts);
     }
 
     @PostMapping
     public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data) {
-        Product newProduct = new Product(data);
-        repository.save(newProduct);
+        productService.registerProduct(data);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping
-    @Transactional
     public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data) {
-        Optional<Product> optionalProduct = repository.findById(data.id());
-        if (optionalProduct.isPresent()) {
-            Product product = optionalProduct.get();
-            product.setName(data.name());
-            product.setPrice_in_cents((data.price_in_cents()));
-            return ResponseEntity.ok(product);
-        } else {
-            throw new EntityNotFoundException();
-        }
+        productService.updateProduct(data);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
-    @Transactional
     public ResponseEntity deleteProduct(@PathVariable String id) {
-        Optional<Product> optionalProduct = repository.findById(id);
-        if (optionalProduct.isPresent()) {
-            Product product = optionalProduct.get();
-            product.setActive(false);
-            return ResponseEntity.noContent().build();
-        } else {
-            throw new EntityNotFoundException();
-        }
+        productService.deleteProduct(id);
+        return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/com/example/crud/services/ProductService.java
@@ -0,0 +1,56 @@
+package com.example.crud.services;
+
+import com.example.crud.domain.product.Product;
+import com.example.crud.domain.product.ProductRepository;
+import com.example.crud.domain.product.RequestProduct;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    private final ProductRepository repository;
+
+    @Autowired
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Product> getAllProducts() {
+        return repository.findAllByActiveTrue();
+    }
+
+    public void registerProduct(RequestProduct data) {
+        Product newProduct = new Product(data);
+        repository.save(newProduct);
+    }
+
+    @Transactional
+    public void updateProduct(RequestProduct data) {
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+
+    @Transactional
+    public void deleteProduct(String id) {
+        Optional<Product> optionalProduct = repository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setActive(false);
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+}
